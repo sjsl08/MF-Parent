ce90b13e22993b9d32a95b4e45f4ea441aa4174c
sjsl08 <lallsimmu80@gmail.com>
2023-12-22 12:20:57 +0530

    starter Config

---
+++ src/app-root-config.ts
@@ -0,0 +1,23 @@
+import { registerApplication, start } from "single-spa";
+import {
+  constructApplications,
+  constructRoutes,
+  constructLayoutEngine,
+} from "single-spa-layout";
+import microfrontendLayout from "./microfrontend-layout.html";
+
+const routes = constructRoutes(microfrontendLayout);
+const applications = constructApplications({
+  routes,
+  loadApp({ name }) {
+    return System.import(name);
+  },
+});
+
+console.log(applications);
+
+const layoutEngine = constructLayoutEngine({ routes, applications });
+
+applications.forEach(registerApplication);
+layoutEngine.activate();
+start();
